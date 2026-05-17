d9592b94204bbcf75ba9bc6449019b2873d4b377
putriintana336-lgtm <putriintana336@gmail.com>
2026-05-18 00:35:16 +0800

    update struktur create blade

--- pinjambarang/resources/views/peminjaman/create.blade.php
+++ pinjambarang/resources/views/peminjaman/create.blade.php
@@ -3,14 +3,6 @@
         <h1 class="text-xl font-bold mb-6">Form Pengajuan Peminjaman</h1>
         <form action="/peminjaman" method="POST" class="flex flex-col gap-4">
             @csrf
-            <select name="barang_id" required class="border border-gray-300 p-2 text-sm bg-white">
-                <option value="">-- Pilih Barang --</option>
-                @foreach($barangs as $b)
-                    <option value="{{ $b->id }}" {{ $b->id == $barang_id ? 'selected' : '' }}>
-                        {{ $b->kode_barang }} - {{ $b->nama_barang }} (Tersedia: {{ $b->jumlah }})
-                    </option>
-                @endforeach
-            </select>
             <label class="text-sm font-bold mt-2">Tanggal Pinjam</label>
             <input type="date" name="tanggal_pinjam" required class="border border-gray-300 p-2 text-sm">
             <label class="text-sm font-bold mt-2">Rencana Tanggal Kembali</label>
